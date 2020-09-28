982754ad9a9eb35a3f676fa9819d6bf5a3efdfc0
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2020-09-28 17:04:11 +0200

    stupid coach

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,14 @@
 require_relative "coach_answer"
+
+answer = nil
+puts ">>"
+while answer != ""
+  puts ">>"
+  user_message = gets.chomp
+  coach_answer(user_message)
+  answer
+end
+
+puts "OK, bye!"
+
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
