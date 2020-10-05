7e9627c27b5a1212da2e538ff9b79b8f9ac21f59
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2020-10-05 11:01:15 +0200

    voting

--- 01-Ruby/02-Flow-Array/01-Can-you-vote/lib/vote.rb
+++ 01-Ruby/02-Flow-Array/01-Can-you-vote/lib/vote.rb
@@ -1,4 +1,9 @@
 def allowed_to_vote?(age)
+  if age >= 18 
+    true
+  else
+    false
+  end
   # TODO: return (not print!) a boolean stating whether `age` is old enough to vote
   # NOTE: Use an if/else statment
 end
