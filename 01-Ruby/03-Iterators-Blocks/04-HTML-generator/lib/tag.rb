7a83571113b13f3361249daa3a4f7c836cf3deab
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2020-10-06 12:39:26 +0200

    yield twice

--- 01-Ruby/03-Iterators-Blocks/04-HTML-generator/lib/tag.rb
+++ 01-Ruby/03-Iterators-Blocks/04-HTML-generator/lib/tag.rb
@@ -1,4 +1,12 @@
 def tag(tag_name, attributes = nil)
-  # TODO: Build HTML tags around content given in the block
-  #       The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
+  text = yield
+  if !attributes.nil?
+    return "<#{tag_name} #{attributes[0]}=\"#{attributes[1]}\">#{text}</#{tag_name}>"
+    # TODO: Build HTML tags around content given in the block
+  else
+    return "<#{tag_name}#{attributes}>#{text}</#{tag_name}>"
+  end
+  # The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
 end
+
+# tag("a", [ "href", "www.google.com"]) { "google it" }
