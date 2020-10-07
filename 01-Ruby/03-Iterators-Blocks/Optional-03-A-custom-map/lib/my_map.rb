fa9d3285ce6018ffec24014e0f35ebd41da3af18
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2020-10-07 10:45:06 +0200

    my map method

--- 01-Ruby/03-Iterators-Blocks/Optional-03-A-custom-map/lib/my_map.rb
+++ 01-Ruby/03-Iterators-Blocks/Optional-03-A-custom-map/lib/my_map.rb
@@ -1,3 +1,7 @@
 def my_map(array)
-  # TODO: Re-implement the same behavior as `Enumerable#map` without using it! You can use `#each` though.
+  new_ary = []
+  array.each do |ele|
+    block_given? ? new_ary << yield(ele) : new_ary << ele
+  end
+  new_ary # TODO: Re-implement the same behavior as `Enumerable#map` without using it! You can use `#each` though.
 end
