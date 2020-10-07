42c1c0d06d6de5d1b73bd982d337350005813e56
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2020-10-07 10:57:21 +0200

    roman string to integer

--- 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
@@ -1,3 +1,32 @@
+ROMAN_TO_INT = {
+  "I" => 1,
+  "IV" => 4,
+  "V" => 5,
+  "IX" => 9,
+  "X" => 10,
+  "XL" => 40,
+  "L" => 50,
+  "XC" => 90,
+  "C" => 100,
+  "CD" => 400,
+  "D" => 500,
+  "CM" => 900,
+  "M" => 1000
+}
+
+# rubocop:disable Metrics/MethodLength
 def roman_to_integer(roman_string)
-  # TODO: translate roman string to integer
+  number = 0
+  str = roman_string.dup # Clone the string to use destructive method on it.
+  until str.size.zero?
+    last_two_characters = str.slice(-2, 2)
+    if ROMAN_TO_INT.key?(last_two_characters)
+      number += ROMAN_TO_INT[last_two_characters]
+      str.chop!
+    else
+      number += ROMAN_TO_INT[str.slice(-1)]
+    end
+    str.chop!
+  end
+  number
 end
